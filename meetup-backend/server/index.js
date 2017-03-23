d75563710c03aa3500a5c596156e9639ac48b3cf
rahullakhaney <rahul@codebrew.io>
2017-03-23 18:46:52 +0400

    created basic server with express

---
+++ meetup-backend/server/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, err => {
+  if (err) {
+    console.error(err);
+  } {
+    console.log(`App listening on port: ${PORT}`);
+  }
+});

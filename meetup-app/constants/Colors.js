f18302f6dfddaef85633c8180061be0c392191e8
rahullakhaney <rahul@codebrew.io>
2017-03-25 19:00:05 +0400

    Added homescreen folder structure

---
+++ meetup-app/constants/Colors.js
@@ -0,0 +1,4 @@
+export default {
+    blackColor: 'black',
+    whiteColor: '#FFF'
+};
--- meetup-app/main.js
+++ meetup-app/main.js
@@ -1,51 +1,14 @@
 import Expo from 'expo';
 import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import Colors from './constants/Colors';
 
-import { fetchMeetups } from './constants/api'
+EStyleSheet.build(Colors);
 
 class App extends React.Component {
-  static defaultProps = {
-    fetchMeetups
-  }
-
-  state = {
-    loading: false,
-    meetups: []
-  }
-
-  async componentDidMount() {
-    this.setState({loading:true});
-    const data = await this.props.fetchMeetups();
-    this.setState({loading:false, meetups: data.meetups});
-  }
-
   render() {
-    if (this.state.loading) {
-      return (
-        <View style={styles.container}>
-          <ActivityIndicator size="large" />
-        </View>
-      )
-    }
-    return (
-      <View style={styles.container}>
-        <Text>MeetupsMe</Text>
-        {this.state.meetups.map((meetup, i) => (
-          <Text key={i}>{meetup.title}</Text>
-        ))}
-      </View>
-    );
+    return <HomeScreen />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 Expo.registerRootComponent(App);

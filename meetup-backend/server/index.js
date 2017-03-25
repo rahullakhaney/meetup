b780e90f638e37a26773841656606b664f086355
rahullakhaney <rahul@codebrew.io>
2017-03-25 11:49:52 +0400

    Added new routes

--- meetup-backend/server/index.js
+++ meetup-backend/server/index.js
@@ -1,31 +1,27 @@
 import express from 'express';
 import dbConfig from './config/db';
-import middlewareConfig from './config/middleware'
-import { MeetupRoutes } from './modules'
+import middlewareConfig from './config/middleware';
+import { MeetupRoutes } from './modules';
 
 const app = express();
 
 /**
-Database
-**/
-
+* Database
+*/
 dbConfig();
-
 /**
-MiddleWare
-**/
-
+* Middlewares
+*/
 middlewareConfig(app);
 
-app.use('api', [MeetupRoutes]);
-
+app.use('/api', [MeetupRoutes]);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, err => {
   if (err) {
     console.error(err);
-  } {
-    console.log(`App listening on port: ${PORT}`);
+  } else {
+    console.log(`App listen to port: ${PORT}`);
   }
 });

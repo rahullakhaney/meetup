aeb51e6d8fa21954e08090b750e6c903494fd6b1
rahullakhaney <rahul@codebrew.io>
2017-03-25 18:23:53 +0400

    Added axios for better API management

--- meetup-app/constants/api.js
+++ meetup-app/constants/api.js
@@ -1,3 +1,3 @@
-export const fetchMeetups = () =>
-  fetch('http://localhost:3000/api/meetups')
-  .then(res => res.json());
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3000/api';

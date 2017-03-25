f9e2358ab5bc2205b72c6d1ff3899f71eda8d441
rahullakhaney <rahul@codebrew.io>
2017-03-25 18:12:16 +0400

    Added GET API for meetups

--- meetup-backend/server/modules/groups/controller.js
+++ meetup-backend/server/modules/groups/controller.js
@@ -72,3 +72,27 @@ export const createGroupMeetup = async (req, res) => {
     return res.status(400).json({ error: true, message: 'Meetup cannot be created!' });
   }
 };
+
+export const getGroupMeetups = async (req, res) => {
+  const { groupId } = req.params;
+
+  if (!groupId) {
+    return res.status(400).json({ error: true, message: 'You need to provided a group id' });
+  }
+
+  // Search for see if group exist
+  const group = await Group.findById(groupId);
+
+  if (!group) {
+    return res.status(400).json({ error: true, message: 'Group not exist' });
+  }
+
+  try {
+    return res.status(200).json({
+      error: false,
+      meetups: await Meetup.find({ group: groupId }).populate('group', 'name')
+    });
+  } catch (e) {
+    return res.status(400).json({ error: true, message: 'Cannot fetch meetup' });
+  }
+};
